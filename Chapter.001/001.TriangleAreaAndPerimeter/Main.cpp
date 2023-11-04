0c2706b85dcd211b7c4777272aea5ee7fd329cd3
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-04 15:20:19 +0200

    add

---
+++ Chapter.001/001.TriangleAreaAndPerimeter/Main.cpp
@@ -0,0 +1,20 @@
+//hapter 001, Task 001
+//29.10.2023, 16:22
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a = 2.3;
+	double b = 3.7;
+	double p, s;
+
+	p = 2 * (a + b);
+	s = a * b;
+
+	cout << "p = " << p << endl;
+	cout << "s = " << s << endl;
+
+	return 0;
+}
