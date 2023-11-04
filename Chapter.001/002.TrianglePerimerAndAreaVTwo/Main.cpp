02ec47f2a689ef031dbb257298dd4f93d6c495d2
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-04 15:21:35 +0200

    add

---
+++ Chapter.001/002.TrianglePerimerAndAreaVTwo/Main.cpp
@@ -0,0 +1,25 @@
+//Chapter 001, Task 002
+//29.10.2023, 17:05
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b;
+
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+
+	double p, s;
+
+	p = 2 * (a + b);
+	s = a * b;
+
+	cout << "p = " << p << endl;
+	cout << "s = " << s << endl;
+
+	return 0;
+}
