e89f64a7d647b2a472ae55091f2d0079e320a5dc
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-05 19:08:08 +0200

    add

---
+++ Chapter.002/Task.001/Main.cpp
@@ -0,0 +1,21 @@
+//Chapter 002, Task 001
+//31.10.2023, 00:27
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int y;
+	double x;
+
+	cout << "x = ";
+	cin >> x;
+
+	y = floor(x);
+
+	cout << y << endl;
+
+	return 0;
+}
