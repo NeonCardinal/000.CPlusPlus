c14ceca97b51b68b966dbddf997ff267a1bfe8d2
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-11 12:39:24 +0200

    add

---
+++ Chapter.007/Problem.003/Main.cpp
@@ -0,0 +1,38 @@
+//Chapter 007, Task 003
+//05.11.2023, 20:18
+#include <iostream>
+
+using namespace std;
+
+void swapi(double& x, double& y)
+{
+	double work = x;
+	x = y;
+	y = work;
+}
+
+int main()
+{
+	double a;
+	double b;
+	double c;
+	double d;
+
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	cout << "d = ";
+	cin >> d;
+
+	cout << "a = " << a << ", b = " << b << ", c = " << c << ", d = " << d << endl;
+
+	swapi(a, b);
+	swapi(c, d);
+
+	cout << "After swapi: a = " << a << ", b = " << b << ", c = " << c << ", d = " << d << endl;
+
+	return 0;
+}
