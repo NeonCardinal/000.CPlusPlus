87eda60945592b7ade62acd67599297f9967062b
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-21 00:10:55 +0200

    add

---
+++ 001.FirstStepsInProgrammingLab/002.Expression/Main.cpp
@@ -0,0 +1,12 @@
+//Chapter First steps in programmin - lab, Task 002
+//20.11.2023, 23:36
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3 << endl;
+
+	return 0;
+}
