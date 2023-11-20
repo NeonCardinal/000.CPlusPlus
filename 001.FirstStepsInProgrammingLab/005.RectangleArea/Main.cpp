c57995279fa85c50ad1dab3df7783d8552d12c31
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-21 00:11:47 +0200

    add

---
+++ 001.FirstStepsInProgrammingLab/005.RectangleArea/Main.cpp
@@ -0,0 +1,18 @@
+//Chapter First steps in programming - lab, Task 005
+//20.11.2023, 23:48
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cin >> a;
+	cin >> b;
+
+	cout << a * b << endl;
+
+	return 0;
+}
