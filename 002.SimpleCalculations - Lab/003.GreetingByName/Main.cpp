4f8277f32b4227df42d5fb1ab01777545ba00c4a
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-22 23:40:49 +0200

    add

---
+++ 002.SimpleCalculations - Lab/003.GreetingByName/Main.cpp
@@ -0,0 +1,17 @@
+//Chapter Simple calculations - lab, Task 003
+//22.11.2023, 23:37
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+
+	getline(cin, name);
+
+	cout << "Hello, " << name << "!" << endl;
+
+	return 0;
+}
