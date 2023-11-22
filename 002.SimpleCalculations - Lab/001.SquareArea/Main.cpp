883878cc341001aeab6bb6b93bf74dc0db835c5a
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-22 23:31:44 +0200

    add

---
+++ 002.SimpleCalculations - Lab/001.SquareArea/Main.cpp
@@ -0,0 +1,19 @@
+//Chapter Simple calculations - lab, Task 001
+//22.11.2023, 23:26
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "a = ";
+	cin >> a;
+
+	int area = a * a;
+
+	cout << area << endl;
+
+	return 0;
+}
