cc83a4accfe3d3cf45645c628acb090444bac7fb
NeonCardinal <nvnnaidenov@gmail.com>
2023-11-26 15:17:59 +0200

    add -> need fix

---
+++ Chapter.013/Problem.010/CirList.cpp
@@ -0,0 +1,208 @@
+//Chapter 013, Task 010
+//25.11.2023, 18:48
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+struct elem
+{
+	T inf;
+	elem* link;
+};
+
+template <class T>
+class CirList
+{
+private:
+	elem<T>* start;
+	elem<T>* current;
+	void DeleteList();
+	void CopyList(CirList&);
+public:
+	CirList();
+	CirList(CirList&);
+	~CirList();
+	CirList& operator=(CirList&);
+	void Print();
+	void IterStart(elem<T>* = NULL);
+	elem<T>* Iter();
+	void ToEnd(T&);
+	void Insert(T&);
+	void DeleteElement(elem<T>*, T&);
+};
+
+template <class T>
+CirList<T>::CirList()
+{
+	start = NULL;
+}
+
+template <class T>
+CirList<T>::CirList(CirList& r)
+{
+	CopyList(r);
+}
+
+template <class T>
+CirList<T>::~CirList()
+{
+	DeleteList();
+}
+
+template <class T>
+CirList<T>& CirList<T>::operator=(CirList& r)
+{
+	if (this != &r)
+	{
+		DeleteList();
+		CopyList(r);
+	}
+
+	return *this;
+}
+
+template <class T>
+void CirList<T>::Print()
+{
+	IterStart();
+
+	elem<T>* p = Iter();
+
+	while (p)
+	{
+		cout << p->inf << " ";
+		p = Iter();
+	}
+
+	cout << endl;
+}
+
+template <class T>
+void CirList<T>::IterStart(elem<T>* p)
+{
+	if (p)
+	{
+		current = p;
+	}
+	else
+	{
+		if (!start)
+		{
+			current = NULL;
+		}
+		else
+		{
+			current = start->link;
+		}
+	}
+}
+
+template <class T>
+elem<T>* CirList<T>::Iter()
+{
+	if (!current)
+	{
+		return NULL;
+	}
+
+	elem<T>* p = current;
+
+	if (current == start)
+	{
+		current = NULL;
+	}
+	else
+	{
+		current = current->link;
+	}
+
+	return p;
+}
+
+template <class T>
+void CirList<T>::ToEnd(T& x)
+{
+	Insert(x);
+
+	start = start->link;
+}
+
+template <class T>
+void CirList<T>::Insert(T& x)
+{
+	elem<T>* p = new elem<T>;
+
+	p->inf = x;
+
+	if (start)
+	{
+		p->link = start->link;
+	}
+	else
+	{
+		start = p;
+	}
+
+	start->link = p;
+}
+
+template <class T>
+void CirList<T>::DeleteElement(elem<T>* p, T& x)
+{
+	x = p->inf;
+
+	if (start != start->link)
+	{
+		elem<T>* q = start;
+
+		while (q->link != p)
+		{
+			q = q->link;
+		}
+
+		q->link = p->link;
+
+		if (p == start)
+		{
+			start = q;
+		}
+
+		delete p;
+	}
+	else
+	{
+		start = NULL;
+		delete p;
+	}
+}
+
+template <class T>
+void CirList<T>::DeleteList()
+{
+	IterStart();
+
+	elem<T>* p = Iter();
+
+	while (p)
+	{
+		delete p;
+		p = Iter();
+	}
+}
+
+template <class T>
+void CirList<T>::CopyList(CirList& r)
+{
+	start = NULL;
+
+	r.IterStart();
+
+	elem<T>* p = r.Iter();
+
+	while (p)
+	{
+		ToEnd(p->inf);
+		p = Iter();
+	}
+}
