62689e83e0a7cc605010ac5979fcc3bcce971a9c
NeonCardinal <nvnnaidenov@gmail.com>
2023-12-10 22:34:45 +0200

    add

---
+++ Chapter.015/Problem.001/Main.cpp
@@ -0,0 +1,62 @@
+//Chapter 015, Task 001
+//10.12.2023, 21:53
+#include <iostream>
+
+using namespace std;
+
+class People
+{
+private:
+	char* name;
+	char* egn;
+public:
+	void ReadPeople(char*, char*);
+	void PrintPeople() const;
+};
+
+void People::ReadPeople(char* str, char* num)
+{
+	name = new char[strlen(str + 1)];
+	strcpy(name, str);
+	egn = new char[11];
+	strcpy(egn, num);
+}
+
+void People::PrintPeople() const
+{
+	cout << "Imena: " << name << endl;
+	cout << "Egn: " << egn << endl;
+}
+
+class Student : People
+{
+private:
+	long facnom;
+	double usp;
+public:
+	void ReadStudent(char*, char*, long, double);
+	void PrintStudent() const;
+};
+
+void Student::ReadStudent(char* str, char* num, long factnum, double uspe)
+{
+	ReadPeople(str, num);
+	facnom = factnum;
+	usp = uspe;
+}
+
+void Student::PrintStudent() const
+{
+	PrintPeople();
+	cout << "facul. nomer: " << facnom << endl;
+	cout << "uspeh: " << usp << endl;
+}
+
+int main()
+{
+	Student stud;
+	stud.ReadStudent("Pesho Peshov", "8618161325", 42444, 6);
+	stud.PrintStudent();
+
+	return 0;
+}
